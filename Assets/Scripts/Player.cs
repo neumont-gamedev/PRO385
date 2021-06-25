c33a221680e4743262385c258200f6ac99653214
neumont-gamedev <rmaple@neumont.edu>
2021-06-25 16:46:55 -0600

    Controller Assignment

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,34 +10,62 @@ public class Player : MonoBehaviour
 
     Vector2 input;
 
+    private void Start()
+    {
+        GetComponent<PlayerInput>().onActionTriggered += HandleAction;
+    }
+
     void Update()
     {
         Gamepad gamepad = Gamepad.current;
         if (gamepad == null) return;
 
         // movement
-        //input = gamepad.leftStick.ReadValue();
-
+        // (input manager)
         //input.x = Input.GetAxis("Horizontal");
         //input.y = Input.GetAxis("Vertical");
+        //// (input system)
+        //input = gamepad.leftStick.ReadValue();
 
         transform.Translate(input * speed * Time.deltaTime);
 
         // fire
+        // (input manager)
         //if (Input.GetButtonDown("Fire1"))
-        if (gamepad.buttonSouth.wasPressedThisFrame)
-		{
-            //GameObject go = Instantiate(shot, transform.position, Quaternion.identity);
-		}
+        //{
+        //    GameObject go = Instantiate(shot, transform.position, Quaternion.identity);
+        //}
+        //// (input system)
+        //if (gamepad.buttonSouth.wasPressedThisFrame)
+        //{
+        //    GameObject go = Instantiate(shot, transform.position, Quaternion.identity);
+        //}
     }
 
-    public void OnFire()
-	{
+    private void OnFire()
+    {
         GameObject go = Instantiate(shot, transform.position, Quaternion.identity);
     }
 
-	public void OnMove(InputValue inputValue)
-	{
+    private void OnMove(InputValue inputValue)
+    {
         input = inputValue.Get<Vector2>();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+    }
+
+    private void HandleAction(InputAction.CallbackContext context)
+    {
+        if (context.action.name == "Fire")
+        {
+            OnFire();
+        }
+        if (context.action.name == "Move")
+		{
+            OnMove(context);
+        }
     }
 }
