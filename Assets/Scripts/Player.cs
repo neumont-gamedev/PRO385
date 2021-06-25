0f5c68355e8316697f6342486ac52c92740c59a2
neumont-gamedev <rmaple@neumont.edu>
2021-06-25 09:33:45 -0600

    Input System Implementation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [Range(1, 10)] public float speed = 2;
     public GameObject shot;
 
-    void Start()
-    {
-        
-    }
+    Vector2 input;
 
     void Update()
     {
-        Vector2 input;
-        input.x = Input.GetAxis("Horizontal");
-        input.y = Input.GetAxis("Vertical");
-        //Debug.Log(input);
+        Gamepad gamepad = Gamepad.current;
+        if (gamepad == null) return;
+
+        // movement
+        //input = gamepad.leftStick.ReadValue();
+
+        //input.x = Input.GetAxis("Horizontal");
+        //input.y = Input.GetAxis("Vertical");
+
         transform.Translate(input * speed * Time.deltaTime);
 
-        if (Input.GetButtonDown("Fire1"))
+        // fire
+        //if (Input.GetButtonDown("Fire1"))
+        if (gamepad.buttonSouth.wasPressedThisFrame)
 		{
-            GameObject go = Instantiate(shot, transform.position, Quaternion.identity);
+            //GameObject go = Instantiate(shot, transform.position, Quaternion.identity);
 		}
+    }
+
+    public void OnFire()
+	{
+        GameObject go = Instantiate(shot, transform.position, Quaternion.identity);
+    }
+
+	public void OnMove(InputValue inputValue)
+	{
+        input = inputValue.Get<Vector2>();
     }
 }
